ccde3859876d7f4c16217e353c853e14736d944c
Randy Swaty <39503604+rswaty@users.noreply.github.com>
2020-08-05 12:18:42 -0400

    modified Climate Change paged based on Kim's comments; added climate change Slickr

---
+++ slicktest/climateChangeSlick.R
@@ -0,0 +1,7 @@
+
+# with slides from Kim Hall
+library(slickR)
+
+climateImages <- c("cc1.JPG", "cc2.JPG", "cc3.JPG", "cc4.JPG", "cc5.JPG", "cc6.JPG")
+climateSlick <- slickR(obj = climateImages, height=350, width='100%') 
+climateSlick
