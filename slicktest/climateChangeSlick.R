1e7345b56bab49fe77eb595338e45491dbc60dab
Randy Swaty <39503604+rswaty@users.noreply.github.com>
2020-08-05 17:11:14 -0400

    changed climate change slickR

--- slicktest/climateChangeSlick.R
+++ slicktest/climateChangeSlick.R
@@ -2,6 +2,6 @@
 # with slides from Kim Hall
 library(slickR)
 
-climateImages <- c("cc1.JPG", "cc2.JPG", "cc3.JPG", "cc4.JPG", "cc5.JPG", "cc6.JPG")
+climateImages <- c("cc1b.JPG", "cc2b.JPG", "cc3b.JPG", "cc4b.JPG", "cc5b.JPG", "cc6b.JPG")
 climateSlick <- slickR(obj = climateImages, height=350, width='100%') 
 climateSlick
